7e594f2bb705867d8a659c1fe9c8645d19cce6a9
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-01 22:21:27 -0600

    add imperial to units param

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,9 +1,9 @@
 class WeatherService
   def get_json(location)
-    response = conn.get("/data/2.5/onecall", {lat: location[:lat], lon: location[:lng], exclude: 'minutely', appid: ENV['OPEN_WEATHER_API_KEY']})
+    response = conn.get("/data/2.5/onecall", {lat: location[:lat], lon: location[:lng], exclude: 'minutely', appid: ENV['OPEN_WEATHER_API_KEY'], units: 'imperial'})
     parsed = JSON.parse(response.body, symbolize_names: true)
   end
-  
+
   private
 
   def conn
