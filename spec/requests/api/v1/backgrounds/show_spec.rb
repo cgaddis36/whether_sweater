f709ff77c3372d60e37b25ef8b52564141b2a1bc
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-01 23:37:52 -0600

    instantiate vcr for web calls

--- spec/requests/api/v1/backgrounds/show_spec.rb
+++ spec/requests/api/v1/backgrounds/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'as a user' do
-  it 'I can retrieve a background image to fit the current weather' do
+  it 'I can retrieve a background image to fit the current weather', :vcr do
 
     get '/api/v1/backgrounds?location=denver,co'
 
--- spec/requests/api/v1/weather/index_spec.rb
+++ spec/requests/api/v1/weather/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "As a frontend developer" do
-  it "I can access the forecast for a city if I send in the params of city_name and state" do
+  it "I can access the forecast for a city if I send in the params of city_name and state", :vcr do
 
     get "/api/v1/forecast?location=denver,co"
 
