be2680ed1626c80f5b3a6af26a1e6550bacb8f35
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-01 20:58:50 -0600

    move conn to private method

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -3,6 +3,8 @@ class WeatherService
     response = conn.get("/data/2.5/onecall", {lat: location[:lat], lon: location[:lng], exclude: 'minutely', appid: ENV['OPEN_WEATHER_API_KEY']})
     parsed = JSON.parse(response.body, symbolize_names: true)
   end
+  
+  private
 
   def conn
     Faraday.new(url: 'https://api.openweathermap.org')
