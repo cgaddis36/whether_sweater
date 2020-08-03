6c6939f4b32f6c0b73d42bfa6d1cb36a1cdfc805
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-03 11:39:01 -0600

    fix test for correct JSON format

--- spec/requests/api/v1/trails/index_spec.rb
+++ spec/requests/api/v1/trails/index_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 describe 'as a developer' do
-  it 'I can retrieve a list of trails for a current location' do
+  it 'I can retrieve a list of trails for a current location', :vcr do
 
     get '/api/v1/trails?location=denver,co'
 
     parsed_data = JSON.parse(response.body, symbolize_names: true)
 
-    expect(parsed_data[:location]).to eq('denver,co')
-    expect(parsed_data[:current_temp]).to_not be_nil
-    expect(parsed_data[:description]).to_not be_nil
-    expect(parsed_data[:trails].count).to eq(10)
-    expect(parsed_data[:trails][0][:difficulty]).to_not be_nil
+    expect(parsed_data[:data][:attributes][:location]).to eq('denver,co')
+    expect(parsed_data[:data][:attributes][:current_temp]).to_not be_nil
+    expect(parsed_data[:data][:attributes][:description]).to_not be_nil
+    expect(parsed_data[:data][:attributes][:trails].count).to eq(10)
+    expect(parsed_data[:data][:attributes][:trails][0][:difficulty]).to_not be_nil
   end
 end
