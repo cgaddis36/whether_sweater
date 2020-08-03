d03d80247bc83e0e67802609344519d9d49a3da0
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-03 11:44:21 -0600

    move facade service actions to private methods

--- app/facades/trails_facade.rb
+++ app/facades/trails_facade.rb
@@ -4,24 +4,26 @@ class TrailsFacade
   end
 
   def serialized_json
-    forecast = parsed_forecast
-    temp = forecast["data"]["attributes"]["current"]["temp"]
-    summary = forecast["data"]["attributes"]["current"]["weather"][0]["description"]
-    service = TrailsService.new
-    trails = service.get_json(@location.downcase)
-    current_forecast = { location: @location, current_temp: temp.to_s, description: summary }
-    current_forecast[:trails] = trails.to_a
     roadtrip = Roadtrip.new(current_forecast)
     x = RoadtripSerializer.new(roadtrip)
-    # current = current_forecast.to_json
-    # trails = trails.to_json
-    # x = JSON.parse(current)
-    # y = JSON.parse(trails)
-    # x[:trails] = y
-    # x
   end
 
   private
+
+  def current_forecast
+    forecast = parsed_forecast
+    temp = forecast["data"]["attributes"]["current"]["temp"]
+    summary = forecast["data"]["attributes"]["current"]["weather"][0]["description"]
+    current = { location: @location, current_temp: temp.to_s, description: summary }
+    trails = trails_service
+    current[:trails] = trails.to_a
+    current
+  end
+
+  def trails_service
+    service = TrailsService.new
+    trails = service.get_json(@location.downcase)
+  end
 
   def parsed_forecast
     forecast =  ForecastFacade.new(@location).serialized_json
