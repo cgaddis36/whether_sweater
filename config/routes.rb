e3b8c6a19a31abfad90fdb746518737acb9aff09
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-03 09:44:37 -0600

    /trails route config

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'weather#index'
       get '/backgrounds', to: 'backgrounds#show'
+      get '/trails', to: 'trails#show'
     end
   end
 end
