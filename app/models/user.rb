cfe3adb76e5b22d7626e2c60fa4b016b94396423
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-04 10:58:24 -0600

    add uniqueness validations for user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates_presence_of :email, :api_key
   
+  validates :email, uniqueness: true, presence: true
+
   validates_confirmation_of :password, :message => "Passwords should match"
 
   has_secure_password
