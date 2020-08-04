c5294b29ff1663acb00df8c02dd8547ad199e5a1
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-04 17:00:33 -0600

    refactor forecast facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -4,23 +4,22 @@ class ForecastFacade
   end
 
   def serialized_json
-    weather = Forecast.new(forecast)
     ForecastSerializer.new(weather)
   end
 
   private
 
-  def forecast
-    weather_service
+  def weather
+    Forecast.new(weather_service)
   end
 
   def weather_service
     service = WeatherService.new
-    parsed_data = service.get_json(location_service)
+    service.get_json(location_service)
   end
 
   def location_service
-    service = LocationService.new
-    parsed = service.format_coordinates(@location.downcase)
+    service = MapService.new
+    service.format_coordinates(@location.downcase)
   end
 end
