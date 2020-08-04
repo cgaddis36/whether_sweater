0709014d272f64274346f6dffc6ae1a45eb6483d
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-04 11:29:39 -0600

    create sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:email])
+   if user && user.authenticate(params[:password])
+     session[:user] = user.id
+     render json: UsersSerializer.new(user)
+   elsif user.nil?
+     render json: "Email does not exist in the database".to_json, status: :bad_request
+   elsif !user.authenticate(params[:password])
+     render json: "Incorrect password".to_json, status: :bad_request
+   end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end
