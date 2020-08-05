8d74e69a55981cde2b2b6d08fa40e585340eb44d
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-05 07:13:18 -0600

    refactor road_trip facade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -18,15 +18,17 @@ class RoadTripFacade
     MapService.new.get_time(@payload)
   end
 
-  def parse_forecast(forecast)
-    JSON.parse(forecast.to_json)
+  def get_future_temp(travel_time)
+    forecast = destination_forecast
+    temp_closest_time = forecast["data"]["attributes"]["current"]["dt"] + travel_time
+    closest_time = forecast["data"]["attributes"]["hourly"].min_by {|data| (temp_closest_time - data["dt"]).abs}
   end
 
-  def get_future_temp(travel_time)
-    forecast = parse_forecast(ForecastFacade.new(@payload["destination"]).serialized_json)
-    temp_closest_time = forecast["data"]["attributes"]["current"]["dt"] + travel_time
-    hourly_times = forecast["data"]["attributes"]["hourly"].map {|data| data["dt"]}
-    x = hourly_times.min_by{|t| (temp_closest_time-t).abs}
-    forecast["data"]["attributes"]["hourly"].find {|data| data["dt"] == x}
+  def destination_forecast
+    parse_forecast(ForecastFacade.new(@payload["destination"]).serialized_json)
+  end
+
+  def parse_forecast(forecast)
+    JSON.parse(forecast.to_json)
   end
 end
