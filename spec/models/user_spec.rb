26dcbfe7e683244dcad3cca0311045393fd723a9
Chase.Gaddis <cgaddis36@gmail.com>
2020-08-14 11:09:11 -0600

    comment test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe User, type: :model do
   it 'validations' do
     should { validate_uniqueness_of :email }
     should { validate_uniqueness_of :api_key }
+    #this is a comment
   end
 end
